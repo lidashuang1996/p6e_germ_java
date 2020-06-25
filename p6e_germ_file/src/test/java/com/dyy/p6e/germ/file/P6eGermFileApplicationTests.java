f119918695b2536928466a29fbaf9b0932108411
lidashuang <1294935733@qq.com>
2020-06-25 17:05:20 +0800

    初次提交 Oauth2 的项目

--- p6e_germ_file/src/test/java/com/dyy/p6e/germ/file/P6eGermFileApplicationTests.java
+++ p6e_germ_file/src/test/java/com/dyy/p6e/germ/file/P6eGermFileApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileApplicationTests {
+class P6eGermFileApplicationTests {
 
     @Test
     void contextLoads() {
