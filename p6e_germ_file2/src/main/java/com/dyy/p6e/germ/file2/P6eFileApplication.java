56b41d24e0a5b203da828efb56e03620f4a5b410
lidashuang <1294935733@qq.com>
2020-11-09 19:29:22 +0800

    提交

---
+++ p6e_germ_file2/src/main/java/com/dyy/p6e/germ/file2/P6eFileApplication.java
@@ -0,0 +1,19 @@
+package com.dyy.p6e.germ.file2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lidashuang
+ * @version 1.0
+ * 1. 文件认证
+ * 2. 文件携带参数截取
+ */
+@SpringBootApplication
+public class P6eFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P6eFileApplication.class, args);
+    }
+
+}
---
+++ p6e_germ_file2/src/test/java/com/dyy/p6e/germ/file2/File2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyy.p6e.germ.file2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class File2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
