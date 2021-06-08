1c245ee4ac89a1b2cc55eab047d5b40e4acb81ed
lidashuang <1294935733@qq.com>
2021-06-08 17:53:02 +0800

    5555

---
+++ p6e-germ-oauth2-2/src/main/resources/static/precache-manifest.3cd3abae862dbd3c001df360bd06b9b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7d8015ddd51d351b26",
+    "url": "css/app.b586740f.css"
+  },
+  {
+    "revision": "52a018f6ef52dd85b9d1",
+    "url": "css/chunk-vendors.32108b03.css"
+  },
+  {
+    "revision": "6375cc85dd97551211b3a112a9e9fb46",
+    "url": "img/logo.6375cc85.png"
+  },
+  {
+    "revision": "9781fab08bf9b40fad5beee5ecd38a1d",
+    "url": "img/sign_bg.9781fab0.jpeg"
+  },
+  {
+    "revision": "c259d823435c4b1e765e326a86edf2e3",
+    "url": "index.html"
+  },
+  {
+    "revision": "7d7d8015ddd51d351b26",
+    "url": "js/app.c0118082.js"
+  },
+  {
+    "revision": "52a018f6ef52dd85b9d1",
+    "url": "js/chunk-vendors.2e63f615.js"
+  },
+  {
+    "revision": "895a463e486e50ee8790ad79a8e98544",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "82865c1b54c49184b4f30b03e241f8f0",
+    "url": "moke/login_account_password.json"
+  },
+  {
+    "revision": "82865c1b54c49184b4f30b03e241f8f0",
+    "url": "moke/login_code.json"
+  },
+  {
+    "revision": "82865c1b54c49184b4f30b03e241f8f0",
+    "url": "moke/login_send_code.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
